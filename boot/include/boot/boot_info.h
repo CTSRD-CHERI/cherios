4f448a01a112834f727d309bd66dede242468ae7
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2016-11-17 22:58:04 +0000

    Add a currently unused boot_info definition shared between boot and kernel.

---
+++ boot/include/boot/boot_info.h
@@ -0,0 +1,49 @@
+/*-
+ * Copyright (c) 2016 SRI International
+ * All rights reserved.
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory under DARPA/AFRL contract FA8750-10-C-0237
+ * ("CTSRD"), as part of the DARPA CRASH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _BOOT_INFO_H_
+#define _BOOT_INFO_H_
+
+#include "cheric.h"
+
+/*
+ * Information populated by boot-loader, and given to the kernel via a
+ * pointer in $c3.
+ */
+struct boot_info {
+	void		*kernel_start_addr;	/* Lowest kernel memory address */
+	uint64_t	kernel_mem_size;	/* Size of contiguous memory for loaded kernel */
+
+	void		*init_start_addr;	/* Lowest init memory address */
+	uint64_t	init_mem_size;		/* Size of contiguous memory for loaded init */
+	reg_frame_t	init_frame;		/* Initial frame for initial activation */
+};
+
+#endif /* _BOOT_INFO_H_ */
