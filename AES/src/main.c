68d6dfc3d4cabde8dfc5b929e21bd148eee5ea94
hx242 <hx242@cam.ac.uk>
2016-12-20 13:20:18 +0000

    Reformat the printing of messages.

--- AES/src/main.c
+++ AES/src/main.c
@@ -287,12 +287,13 @@ exit:
 }
 
 int main() {
+    printf("AES Hello World.\n");
     int ret = namespace_register(5, act_self_ref, act_self_id);
     if(ret!=0) {
         printf("AES: register failed\n");
         return -1;
     }
+    printf("AES: register OK\n");
     msg_enable = 1;
-    printf("AES Hello World.\n");
     return 0;
 }
--- sha/src/main.c
+++ sha/src/main.c
@@ -15,12 +15,13 @@ size_t ctrl_methods_nb = countof(ctrl_methods);
 
 int main()
 {
+    printf("SHA Hello World.\n");
     int ret = namespace_register(6, act_self_ref, act_self_id);
     if(ret!=0) {
         printf("SHA: register failed\n");
         return -1;
     }
+    printf("SHA: register OK\n");
     msg_enable = 1;
-    printf("SHA Hello World.\n");
     return(0);
 }
