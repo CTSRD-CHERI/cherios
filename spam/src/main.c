e5d38e08c7adff319824ca675f500d205ca15320
Hongyan Xia <Jerryxia32@gmail.com>
2017-01-05 15:04:56 +0000

    spam only sends EACH_BLOCK_SIZE to AES at a time, creating a lot of domain transitions.

--- spam/src/main.c
+++ spam/src/main.c
@@ -6,6 +6,8 @@
 #include<assert.h>
 #include<sha_info.h>
 
+#define EACH_BLOCK_SIZE 256
+
 extern char __AES_start, __AES_end;
 
 int
@@ -22,18 +24,27 @@ main() {
 	assert(sha_ref != NULL);
 	void * sha_id  = namespace_get_id(6);
 
-    int64_t len = &__AES_end - &__AES_start;
+    size_t len = &__AES_end - &__AES_start;
+    size_t encdecOffset = 0, totalDeced = 0, remain = 0;
+    int64_t encret;
+    uint64_t decret;
 
-    uint8_t *enc = (uint8_t *)malloc((size_t)len+32);
-    uint8_t *encdec = (uint8_t *)malloc((size_t)len+32);
+    uint8_t *enc = (uint8_t *)malloc(EACH_BLOCK_SIZE + 32);
+    uint8_t *encdec = (uint8_t *)malloc((size_t)len + 32);
 
     const char *theKey = "0123456789ABCDEFFEDCBA98765432100123456789ABCDEFFEDCBA9876543210";
 
-    int64_t encret = ccall_rrrr_r(u_ref, u_id, 0, (register_t)&__AES_start, (register_t)enc, len, (register_t)theKey);
-    printf("Encryption length: %ld, ended with %ld status.\n", len, encret);
+    while((remain = len-encdecOffset) > EACH_BLOCK_SIZE) {
+        encret = ccall_rrrr_r(u_ref, u_id, 0, ((size_t)&__AES_start + encdecOffset), (register_t)enc, EACH_BLOCK_SIZE, (register_t)theKey);
+        decret = ccall_rrrr_r(u_ref, u_id, 0, (register_t)enc, (size_t)encdec + totalDeced, -encret, (register_t)theKey);
+        encdecOffset += EACH_BLOCK_SIZE;
+        totalDeced += decret;
+    }
+    encret = ccall_rrrr_r(u_ref, u_id, 0, ((size_t)&__AES_start + encdecOffset), (register_t)enc, remain, (register_t)theKey);
+    decret = ccall_rrrr_r(u_ref, u_id, 0, (register_t)enc, (size_t)encdec + totalDeced, -encret, (register_t)theKey);
+    totalDeced += decret;
 
-    uint64_t decret = ccall_rrrr_r(u_ref, u_id, 0, (register_t)enc, (register_t)encdec, -encret, (register_t)theKey);
-    printf("Decryption ended with %ld status.\n", decret);
+    printf("Size of the original: %ld, Total bytes decrypted: %ld\n", len, totalDeced);
 
     SHA_INFO theinfo;
     ccall_rrr_n(sha_ref, sha_id, 0, (register_t)&theinfo, (register_t)&__AES_start, (size_t)len);
