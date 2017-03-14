CTSRD-CHERI/cherios

commit 0e7a297fa1b5de813cf465124bb8a739e4f2e27c

Avoid using memcmp(); it's not implemented.

boot/src/boot/elf_loader.c
@@ -123,7 +123,10 @@ typedef  struct
 }  Elf64_Phdr;
 
 int elf_check_supported(Elf64_Ehdr *hdr) {
-	if(memcmp(hdr->e_ident, "\x7F""ELF", 4)) {
+	if (hdr->e_ident[0] != 0x7f ||
+	    hdr->e_ident[1] != 'E' ||
+	    hdr->e_ident[1] != 'L' ||
+	    hdr->e_ident[1] != 'F') {
 		ERROR("Bad magic number");
 		return 0;
 	}

include/string.h
@@ -34,7 +34,6 @@
 #include "mips.h"
 
 void	bzero(void *, size_t);
-int	memcmp(const void * cs,const void * ct,size_t count);
 void *	memcpy(void *dest, const void *src, size_t n);
 void *	memset(void *, int, size_t);
 char *	strchr(const char * s, int c);

commit ca86fb7c9263473aa2b52736db0bd22fd992c1a0

Fix thinko introduced in 0e7a297fa1b5de813cf465124bb8a739e4f2e27c.

boot/src/boot/elf_loader.c
@@ -125,8 +125,8 @@ typedef  struct
 int elf_check_supported(Elf64_Ehdr *hdr) {
 	if (hdr->e_ident[0] != 0x7f ||
 	    hdr->e_ident[1] != 'E' ||
-	    hdr->e_ident[1] != 'L' ||
-	    hdr->e_ident[1] != 'F') {
+	    hdr->e_ident[2] != 'L' ||
+	    hdr->e_ident[3] != 'F') {
 		ERROR("Bad magic number");
 		return 0;
 	}