97af06d4307a8182fa692fd600ae2d0cae441ae7
Lawrence Esswood <le277@cam.ac.uk>
2017-06-13 18:04:24 +0100

    added missing files

---
+++ include/macroutils.h
@@ -0,0 +1,89 @@
+/*-
+ * Copyright (c) 2017 Lawrence Esswood
+ * All rights reserved.
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
+ * ("CTSRD"), as part of the DARPA CRASH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef CHERIOS_MACROUTILS_H
+#define CHERIOS_MACROUTILS_H
+
+
+/* e.g. where X = (int, a, int, b)
+ * MAKE_SIG(X) -> (int a, int b)
+ * MAKE_ARG_LIST(X) -> a, b
+ * MAKE_ARG_LIST_APPEND(X) -> , a, b
+ */
+#define MAKE_SIG(X) MS_HELP X
+#define MAKE_ARG_LIST(X) MAL_HELP(MAL_start, EVAL5 X)
+#define MAKE_ARG_LIST_APPEND(X) MAL_HELP(MAL_start_c, EVAL5 X)
+
+
+
+#define MAKE_CTR(NAME) struct _CTR_TYPE_ ## NAME ## _start {char ofsize[__COUNTER__+1];};
+#define CTR(NAME)       (__COUNTER__  - sizeof(struct _CTR_TYPE_ ## NAME ## _start))
+
+
+#define EVAL(...)  EVAL1(EVAL1(EVAL1(__VA_ARGS__)))
+#define EVAL1(...) EVAL2(EVAL2(EVAL2(__VA_ARGS__)))
+#define EVAL2(...) EVAL3(EVAL3(EVAL3(__VA_ARGS__)))
+#define EVAL3(...) EVAL4(EVAL4(EVAL4(__VA_ARGS__)))
+#define EVAL4(...) EVAL5(EVAL5(EVAL5(__VA_ARGS__)))
+#define EVAL5(...) __VA_ARGS__
+
+#define EMPTY(...)
+#define DEFER(...) __VA_ARGS__ EMPTY()
+
+#define MULTI32(X) X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X
+#define ANY_HELP(...) ANY(__VA_ARGS__)
+#define ANY(_1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
+     _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
+     _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
+     _31,_32, _33, X, ...) X
+#define SELECT_REC(BASE, REC, ...) ANY_HELP(__VA_ARGS__, MULTI32(REC), BASE) (__VA_ARGS__)
+#define SELECT_REC_IND() SELECT_REC
+
+#define MAL_void(X)
+
+#define MAL_base(name) name
+#define MAL_rec_ind() MAL_rec
+#define MAL_rec(name, typenext, ...) name, DEFER(SELECT_REC_IND)()(MAL_base, DEFER(MAL_rec_ind)(), __VA_ARGS__)
+#define MAL_start(type, ...) DEFER(SELECT_REC_IND)()(MAL_base, DEFER(MAL_rec_ind)(), __VA_ARGS__)
+
+#define MAL_start_c(type, ...) , DEFER(SELECT_REC_IND)()(MAL_base, DEFER(MAL_rec_ind)(), __VA_ARGS__)
+
+#define MAL_HELP(F, ...) EVAL(SELECT_REC(MAL_void, F, __VA_ARGS__))
+
+#define MS_void(X)  void
+
+#define MS_base(X) X
+#define MS_rec_ind() MS_rec
+#define MS_rec(name, typenext, ...) name, typenext DEFER(SELECT_REC_IND)()(MS_base, DEFER(MS_rec_ind)(), __VA_ARGS__)
+#define MS_start(type, ...) type DEFER(SELECT_REC_IND)()(MS_base, DEFER(MS_rec_ind)(), __VA_ARGS__)
+#define MS_HELP(...) (EVAL(SELECT_REC(MS_void, MS_start, __VA_ARGS__)))
+
+
+#endif //CHERIOS_MACROUTILS_H
---
+++ include/nano/nano_if_list.h
@@ -0,0 +1,108 @@
+/*-
+ * Copyright (c) 2017 Lawrence Esswood
+ * All rights reserved.
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
+ * ("CTSRD"), as part of the DARPA CRASH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef CHERIOS_NANO_IF_LIST_H
+#define CHERIOS_NANO_IF_LIST_H
+
+#include "macroutils.h"
+
+/* Define the arguments with commas so we can extract just arguments if we want */
+
+#define NANO_KERNEL_IF_RAW_LIST(ITEM, ...)                                          \
+/* TODO in order to do SGX like things we may have an argument that means "and give them a new capability" */\
+/* Creates a context from a intial reg_frame and returns a handle */\
+    ITEM(create_context, context_t, (reg_frame_t*, initial_state), __VA_ARGS__)  \
+/* Deletes a context, restore_from is ONLY used if a context is destroying itself */\
+    ITEM(destroy_context, context_t, (context_t, context, context_t, restore_from), __VA_ARGS__) \
+/* Switch to a handle, and store a handle for the suspended context to the location pointed to by store_to */\
+    ITEM(context_switch, void, (context_t, restore_from, context_t*,  store_to), __VA_ARGS__) \
+/* Delays interrupts until exit is called the same number of times OR context_switch is called */\
+    ITEM(critical_section_enter, void, (void), __VA_ARGS__) \
+    ITEM(critical_section_exit, void, (void), __VA_ARGS__) \
+/* TODO a better interface would consist of two things: A chain of contexts to restore, and a chain per exception type.
+ * TODO my thoughts are these: In the event of an interrupt, it seems silly to restore a special context that decides
+ * TODO who to switch to. If it was a timer interrupt we can just schedule the next person in a list. If it was a
+ * TODO specific asyn interrupt, some activation (which might have a high priority) might be waiting on that. In this
+ * TODO case we might want to restore that context straight away */ \
+    ITEM(set_exception_handler, void, (context_t, context), __VA_ARGS__) \
+/* Returns a proper capability made from a reservation. state open -> taken. Fails if not open */\
+    ITEM(rescap_take, void, (res_t, res, cap_pair*, out), __VA_ARGS__)\
+/* Returns a SEALED version of rescap_take, but does not change state. Then get fields using normal ops */\
+    ITEM(rescap_info, capability, (res_t, res), __VA_ARGS__)\
+/* Tells the collector to start collecting this reservation. collecting fails if already collecting */\
+    ITEM(rescap_collect, res_t, (res_t, res), __VA_ARGS__)\
+/* Splits an open reservation. The reservation will have size `size'. The remaining space will be returned as a new reservation. */\
+    ITEM(rescap_split, res_t, (capability, res, size_t, size), __VA_ARGS__)\
+/* Merges two taken reservations. Cannot merge with collecting. If an open and taken are merged the result is taken*/\
+    ITEM(rescap_merge, res_t, (res_t, res1, res_t, res2), __VA_ARGS__)\
+/* Create a node. Argument must be open, will transition to taken, and a single child will be created and returned*/\
+    ITEM(rescap_parent, res_t, (res_t, res), __VA_ARGS__)\
+/* Get a physical page. Can only be done if the page is not nano owned, or mapped to from a virtual address*/\
+    ITEM(get_phy_page, void, (register_t, page_n, int, cached, register_t, npages, cap_pair*, out), __VA_ARGS__)\
+/* Allocate a physical page to be page table. */\
+    ITEM(create_table, ptable_t, (register_t, page_n, ptable_t, parent, register_t, index),  __VA_ARGS__)\
+/* Map an entry in a leaf page table to a physical page. The adjacent physical page will also be mapped */\
+    ITEM(create_mapping, void, (register_t, page_n, ptable_t, table, register_t, index, register_t, flags),  __VA_ARGS__) \
+/* Free a mapping. This will recover the physical page */\
+    ITEM(free_mapping, void, (ptable_t, table, register_t, index), __VA_ARGS__)   \
+/* Use a reservation to show a virtual range can be remappep */\
+    ITEM(clear_mapping, void, (ptable_t, table, register_t, vaddr, res_t, reservation), __VA_ARGS__)\
+/* Get a handle for the top level page table*/\
+    ITEM(get_top_level_table, ptable_t, (void), __VA_ARGS__) \
+/* Get a handle for a sub table from an L0 or L1 table. */\
+    ITEM(get_sub_table, ptable_t, (ptable_t, table, register_t, index), __VA_ARGS__)\
+/* Get a Read-Only cap for the table */\
+    ITEM(get_read_only_table, readable_table_t*, (ptable_t, table), __VA_ARGS__)\
+/* Create the reservation for all of virtual memory */\
+    ITEM(make_first_reservation, res_t, (void), __VA_ARGS__) \
+/* Get a read only capability to the nano kernels book */\
+    ITEM(get_book, page_t*, (void), __VA_ARGS__) \
+/* Thw page must have a non zero length. Will make its length new length */\
+    ITEM(split_phy_page_range, void, (register_t, pagen, register_t, new_len), __VA_ARGS__)\
+/* The page must have non zero length, and the the next page must have identical status. Merges the records. */\
+    ITEM(merge_phy_page_range, void, (register_t, pagen), __VA_ARGS__)\
+/* FIXME for debug ONLY. When we have proper debugging, this must be removed. It defeats the whole point. */\
+/* For debugging. Returns a global cap and gives your pcc all the permission bits it can */\
+    ITEM(obtain_super_powers, capability, (void), __VA_ARGS__)\
+/* Gets a R/W cap for the userdata space of a meta data node of a reservation. */\
+    ITEM(get_userdata_for_res, capability, (res_t, res), __VA_ARGS__)\
+/* Get the victim context and cause register for the last exception. */\
+    ITEM(get_last_exception, void, (exection_cause_t*, out), __VA_ARGS__)\
+/* For both diagnostic and performance reasons a r.w capability to the EL is available. Use this for a fast critical region*/\
+    ITEM(get_critical_level_ptr, ex_lvl_t*,  (void), __VA_ARGS__)\
+    ITEM(get_critical_cause_ptr, cause_t*,  (void), __VA_ARGS__)
+
+
+
+
+#define RAW_TO_NORMAL(name, ret, raw_sig, X, ...) X(name, ret, MAKE_SIG(raw_sig), __VA_ARGS__)
+
+#define NANO_KERNEL_IF_LIST(ITEM, ...) NANO_KERNEL_IF_RAW_LIST(RAW_TO_NORMAL, ITEM, __VA_ARGS__)
+#endif //CHERIOS_NANO_IF_LIST_H
