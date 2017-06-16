db910a5cc3f3005b6d90c5dd45aa4182fede0f0b
Lawrence Esswood <le277@cam.ac.uk>
2017-06-16 15:52:50 +0100

    Release lock on all code paths

--- cherios/kernel/src/msg.c
+++ cherios/kernel/src/msg.c
@@ -204,9 +204,10 @@ void kernel_message_send(capability c3, capability c4, capability c5, capability
         source_activation->sched_status = sched_runnable;
 		CRITICAL_LOCKED_END(&target_activation->writer_spinlock);
 		sched_reschedule(target_activation, 0);
+	} else {
+		CRITICAL_LOCKED_END(&target_activation->writer_spinlock);
 	}
 
-	CRITICAL_LOCKED_END(&target_activation->writer_spinlock);
 	ret->v0 = 0;
 	ret->v1 = 0;
 	ret->c3 = NULL;
