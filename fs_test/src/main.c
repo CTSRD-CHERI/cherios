226b9a9e81fdd79888d5a0073a9b43c5c8021019
Lawrence Esswood <le277@cam.ac.uk>
2018-04-27 16:45:11 +0100

    Test has more interesting text to make errors obvious

--- fs_test/src/main.c
+++ fs_test/src/main.c
@@ -28,6 +28,7 @@
  * SUCH DAMAGE.
  */
 
+
 #include "cheric.h"
 #include "unistd.h"
 #include "assert.h"
@@ -36,7 +37,24 @@
 
 #define BIG_SIZE 0x1000
 
-char source[BIG_SIZE];
+char LOREM[BIG_SIZE] = "\n"
+"\n"
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut dignissim sapien. In eu nunc et dolor efficitur ultrices. Duis sit amet metus at quam sodales fringilla. Donec consequat felis felis, at iaculis ante malesuada id. Pellentesque metus sapien, cursus ac libero in, dictum fermentum quam. Phasellus scelerisque, purus vel condimentum maximus, arcu sem placerat odio, id lobortis ante orci at purus. Nullam mattis ultricies iaculis. Etiam maximus nunc vitae tempor commodo. Donec vel est velit. Proin eget justo ante.\n"
+"\n"
+"Nam id ornare massa. Nunc tincidunt maximus gravida. Cras nibh nulla, fermentum in enim vitae, luctus ultrices odio. Integer vitae velit ipsum. Vivamus nisi nibh, pulvinar sit amet neque a, blandit pharetra risus. Maecenas nisi felis, sollicitudin consectetur faucibus dignissim, finibus a lacus. Curabitur tempor odio nec aliquam efficitur. Mauris volutpat, turpis a dapibus condimentum, tortor dui tempus dui, ac vulputate lectus turpis nec sem. Donec ac sollicitudin ex. Mauris non augue vitae tortor vulputate egestas. Etiam sed urna nec metus interdum imperdiet. Nulla volutpat massa nec lectus accumsan volutpat. Donec posuere posuere ligula ut fermentum.\n"
+"\n"
+"Nulla pretium ante tortor, nec ultricies est tristique vitae. Nullam metus tellus, malesuada sit amet malesuada rutrum, maximus vel justo. Ut ac rhoncus enim. Maecenas et scelerisque enim. Phasellus quis commodo enim. Integer lobortis enim vitae nisi scelerisque posuere. Integer at laoreet ex.\n"
+"\n"
+"Integer sollicitudin pulvinar diam, eget feugiat arcu congue at. Pellentesque vitae feugiat augue. Vestibulum ut turpis a metus condimentum ullamcorper sit amet non dui. Donec rhoncus hendrerit gravida. Curabitur iaculis velit vitae tempor tempus. Sed mollis ultricies neque non ullamcorper. Mauris vel neque malesuada, porttitor erat vitae, rutrum arcu. Nunc id ante sapien. Vivamus facilisis justo ipsum, ac consequat libero ornare elementum. Cras nec odio eget ligula ornare molestie non ac ex. Aliquam ullamcorper sed velit eu ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras pharetra luctus egestas. Ut quis rhoncus turpis, et imperdiet ligula.\n"
+"\n"
+"Nulla ornare rutrum diam non bibendum. Proin non dapibus orci, et euismod nulla. Praesent bibendum, dolor sed ullamcorper interdum, ipsum lectus ultrices justo, a rutrum sem eros vitae ipsum. Nunc vel sollicitudin mi, a accumsan nisl. Vestibulum in tempus arcu. Fusce condimentum venenatis gravida. Suspendisse lacinia massa in suscipit ornare. Aliquam erat volutpat. Vivamus sed tempor sem.\n"
+"\n"
+"Morbi luctus risus sit amet nibh accumsan elementum. Ut ac quam placerat, pulvinar libero sollicitudin, vehicula tellus. Aenean sed felis vel velit feugiat ullamcorper. Praesent finibus tempor facilisis. Maecenas tincidunt vel mauris et tristique. Donec sit amet ex vel ligula imperdiet condimentum. Fusce maximus risus sit amet purus blandit, vitae mollis sem pulvinar. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In hac habitasse platea dictumst. In euismod viverra congue. Quisque quis varius lectus. Sed congue diam id sapien eleifend ultrices. Duis iaculis sodales porta. Donec pharetra lobortis tellus a egestas.\n"
+"\n"
+"Morbi eros nisl, ullamcorper quis sem non, iaculis efficitur neque. Duis consectetur justo ut sapien viverra, ut facilisis felis pulvinar. Quisque tempor neque justo, eu aliquet massa maximus ut. Praesent sodales vulputate luctus. Sed quis molestie lacus. Maecenas metus mi, aliquet at sem et, ultricies dignissim dolor. Donec pharetra nec diam eu vulputate. Curabitur facilisis auctor luctus. Sed scelerisque mi eu erat facilisis, porttitor iaculis erat tempor. Nullam semper fringilla diam eget venenatis. Curabitur sagittis magna ac eros feugiat maximus. Sed ultrices massa a magna convallis, sit amet aliquet ante molestie. Donec finibus viverra tellus vitae egestas.\n"
+"\n"
+"Curabitur interdum, nunc a ullamcorper aliquet, nibh metus faucibus nulla, ut elementum nisl ante id justo. Vestibulum lacus elit, laoreet sit amet posuere et, rhoncus eu turpis. Curabitur fringaa";
+
 char dest[BIG_SIZE];
 
 int main(register_t arg, capability carg) {
@@ -78,15 +96,13 @@ int main(register_t arg, capability carg) {
 
     assert_int_ex(result, ==, 0);
 
-    for(size_t i = 0; i < BIG_SIZE; i++) {
-        source[i] = 'A';
-    }
+    char chars[] = {'0','1','2'};
 
     file = open("bigtest", 1, 1, MSG_NONE);
 
     assert(file != NULL);
 
-    result = write(file, source, BIG_SIZE);
+    result = write(file, LOREM, BIG_SIZE);
 
     assert_int_ex(-result, ==, -BIG_SIZE);
 
@@ -97,7 +113,7 @@ int main(register_t arg, capability carg) {
     assert_int_ex(result, ==, BIG_SIZE);
 
     for(size_t i = 0; i < BIG_SIZE; i++) {
-        assert_int_ex('A', ==, dest[i]);
+        assert_int_ex(LOREM[i], ==, dest[i]);
         dest[i] = 0;
     }
 
@@ -122,8 +138,20 @@ int main(register_t arg, capability carg) {
     result = read(file2, dest, BIG_SIZE);
     assert_int_ex(result, ==, BIG_SIZE);
 
+    assert_int_ex(file2->sock.read.pull_reader->requeste_ptr, ==, file2->sock.read.pull_reader->fulfiller_component.fulfill_ptr);
+    int once = 1;
     for(size_t i = 0; i < BIG_SIZE; i++) {
-        assert_int_ex('A', ==, dest[i]);
+        if(LOREM[i] != dest[i]) {
+            printf("Problem with %lx\n", i);
+            if(once--) {
+                for(size_t j = 0; j < BIG_SIZE; j++) {
+                    if(j % 16 == 0) printf("\n");
+                    if(j % (16 * 32) == 0) printf("\n");
+                    printf("(%d|%d) ", LOREM[j], dest[j]);
+                }
+            }
+        }
+        assert_int_ex(LOREM[i], ==, dest[i]);
     }
 
     result = close(file2);
