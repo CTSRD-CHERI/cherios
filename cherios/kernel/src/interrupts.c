221384ce63a9b054766e2151e039d12617f1410b
Lawrence Esswood <le277@cam.ac.uk>
2018-05-31 14:23:46 +0100

    Removed buggy shared state in interrupts

--- cherios/kernel/src/interrupts.c
+++ cherios/kernel/src/interrupts.c
@@ -42,9 +42,9 @@ typedef struct interrupt_register_t {
 	register_t arg;
 } interrupt_register_t;
 
-static interrupt_register_t int_child[7];
+static interrupt_register_t int_child[7*SMP_CORES];
 
-register_t handle_mask = 0;
+#define GET_REG(core,id) int_child[id + (core * SMP_CORES)]
 
 /* FIXME This entire thing will break when remove access from the kernel to CP0
  * FIXME the solution will be to have the nano kernel expose a suitable interface
@@ -63,23 +63,23 @@ void kernel_interrupts_init(int enable_timer, uint8_t cpu_id) {
 	}
 }
 
-static void kernel_interrupt_others(register_t pending) {
+static void kernel_interrupt_others(register_t pending, uint8_t cpu_id) {
 	for(uint8_t i=0; i<7; i++) {
 		if(pending & (1<<i)) {
-			if(int_child[i].target == NULL) {
+			interrupt_register_t* registration = &GET_REG(cpu_id,i);
+			if(registration->target == NULL) {
 				KERNEL_ERROR("Interrupt with no handle %d",i);
 				continue;
 			}
             KERNEL_TRACE("interrupt disable", "%d", i);
-			handle_mask = (handle_mask & ~(1<<i));
 			cp0_status_im_disable(1<<i);
 			// FIXME we probabably want a seperate interrupt source from the kernel
 
 			// FIXME this breaks as we can't take a proper TLB fault in exception levels
 			// FIXME I have added a cludge to load every slot on register but this needs fixing
-			if(msg_push(int_child[i].carg, NULL, NULL, NULL,
-						int_child[i].arg, i, 0, 0,
-						int_child[i].v0, int_child[i].target, &kernel_acts[0], NULL)) {
+			if(msg_push(registration->carg, NULL, NULL, NULL,
+						registration->arg, i, 0, 0,
+						registration->v0, registration->target, &kernel_acts[0], NULL)) {
 				kernel_panic("queue full (int)");
 			}
 		}
@@ -88,14 +88,15 @@ static void kernel_interrupt_others(register_t pending) {
 
 void kernel_interrupt(register_t cause, uint8_t cpu_id) {
 	register_t ipending = cp0_cause_ipending_get(cause);
-	register_t toprocess = ipending & (~MIPS_CP0_CAUSE_IP_TIMER) & handle_mask;
-    register_t mask = cp0_status_im_get();
+	register_t mask = cp0_status_im_get();
+	register_t toprocess = ipending & (~MIPS_CP0_CAUSE_IP_TIMER) & mask;
+
 	KERNEL_TRACE("interrupt", "pending: %lx, to_process: %lx cpu: %d ", ipending, toprocess, cpu_id);
 	if (ipending & MIPS_CP0_CAUSE_IP_TIMER) {
 		kernel_timer(cpu_id);
 	}
 	if(toprocess) {
-		kernel_interrupt_others(toprocess);
+		kernel_interrupt_others(toprocess, cpu_id);
 	}
 }
 
@@ -107,26 +108,29 @@ static int validate_number(int number) {
 }
 
 int kernel_interrupt_enable(int number, act_control_t * ctrl) {
+	// FIXME races
     KERNEL_TRACE("interrupt enable", "%d", number);
 	number = validate_number(number);
 	if(number < 0) {
 		return -1;
 	}
-	if(int_child[number].target != ctrl) {
+	uint8_t cpu_id = cp0_get_cpuid();
+	if(GET_REG(cpu_id,number).target != ctrl) {
 		return -1;
 	}
     // This will eventually fail when we can no longer access cp0
-	handle_mask |= (1 << number);
 	cp0_status_im_enable(1<<number);
 	return 0;
 }
 
 int kernel_interrupt_register(int number, act_control_t * ctrl, register_t v0, register_t arg, capability carg) {
+	// FIXME races
 	number = validate_number(number);
 	if(number < 0) {
 		return -1;
 	}
-	if(int_child[number].target != NULL && int_child[number].target != ctrl) {
+	uint8_t cpu_id = cp0_get_cpuid();
+	if(GET_REG(cpu_id,number).target != NULL && GET_REG(cpu_id,number).target != ctrl) {
 		return -1;
 	}
 
@@ -140,9 +144,9 @@ int kernel_interrupt_register(int number, act_control_t * ctrl, register_t v0, r
 	}
 
 
-	int_child[number].target = (act_t*)ctrl;
-	int_child[number].arg = arg;
-	int_child[number].carg = carg;
-	int_child[number].v0 = v0;
+	GET_REG(cpu_id,number).target = (act_t*)ctrl;
+	GET_REG(cpu_id,number).arg = arg;
+	GET_REG(cpu_id,number).carg = carg;
+	GET_REG(cpu_id,number).v0 = v0;
 	return 0;
 }
