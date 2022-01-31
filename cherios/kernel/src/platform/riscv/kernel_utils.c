aaf86f3461aa2040fdd19c8f817a3a8b6275d0a2
Lawrence Esswood <le277@cam.ac.uk>
2022-01-31 14:27:49 +0000

    Add in reset code for QEMU RISCV

--- cherios/kernel/src/platform/riscv/kernel_utils.c
+++ cherios/kernel/src/platform/riscv/kernel_utils.c
@@ -30,10 +30,28 @@
 
 #include "klib.h"
 
-void kernel_platform_init(__unused page_t* book) {
+#define VIRT_TEST_BASE  0x100000
+#define VIRT_TEST_SIZE   0x1000
+
+capability virt_test_cap;
+
+void kernel_platform_init(page_t* book) {
+    virt_test_cap = get_phy_cap(book, VIRT_TEST_BASE, VIRT_TEST_SIZE, 0, 1);
+}
+
+enum sifive_shutdown_status_e {
+    FINISHER_FAIL = 0x3333,
+    FINISHER_PASS = 0x5555,
+    FINISHER_RESET = 0x7777
+};
+
+static void hw_reset_sifive(enum sifive_shutdown_status_e status, uint16_t code) {
+    uint32_t val = (uint32_t)status | ((uint32_t)code << 16);
+    *((volatile uint32_t *)virt_test_cap) = val;
 }
 
 void hw_reboot(void) {
-    // TODO RISCV
+    hw_reset_sifive(FINISHER_RESET, 0);
+    kernel_printf("RISCV: shutdown failed\n");
     for(;;);
 }
